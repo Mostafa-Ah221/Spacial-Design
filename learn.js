3c8cf6a3847c5d9192079c6759cefac40187a6b4
Mostafa Ahmed <mostafaahmedd7755@gmail.com>
2024-10-03 10:52:11 +0300

    Update learn.js

--- learn.js
+++ learn.js
@@ -97,7 +97,7 @@ function randomizeImgs() {
       let randomNumber = Math.floor(Math.random() * imagArrar.length);
       //change backgroun Image Url
       landingPage.style.backgroundImage =
-        'url("./image/' + imagArrar[randomNumber] + '")';
+        'url("./image/'+imagArrar[randomNumber]+'")';
     }, 1000);
     //
   }
